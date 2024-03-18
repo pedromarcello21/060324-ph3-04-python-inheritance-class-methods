2d7f681ef1f34d044ed6c77d6743216f7a1ff5d3
brewchetta <chett.tiller@gmail.com>
2024-03-18 09:26:58 -0400

    mammal methods included in cat and dog

--- classes/cat.py
+++ classes/cat.py
@@ -1,8 +1,18 @@
 class Cat:
 
-    def __init__(self, name, lives_remaining=9):
+    def __init__(self, name, rested, lives_remaining=9):
         self.name = name
+        self.rested = rested
         self.lives_remaining = lives_remaining
 
     def __repr__(self):
-        return f"Cat(name={self.name}, lives_remaining={self.lives_remaining})"
+        return f"Cat(name={self.name}, rested={self.rested}, lives_remaining={self.lives_remaining})"
+
+    def make_sound(self):
+        return "generic mammal sound"
+
+    def sleep(self):
+        self.rested = True
+
+    def run_around(self):
+        self.rested = False
--- classes/dog.py
+++ classes/dog.py
@@ -1,8 +1,18 @@
 class Dog:
 
-    def __init__(self, name, is_good=True):
+    def __init__(self, name, rested, is_good=True):
         self.name = name
+        self.rested = rested
         self.is_good = is_good
 
     def __repr__(self):
-        return f"Dog(name={self.name}, is_good={self.is_good})"
+        return f"Dog(name={self.name}, rested={self.rested}, is_good={self.is_good})"
+
+    def make_sound(self):
+        return "generic mammal sound"
+
+    def sleep(self):
+        self.rested = True
+
+    def run_around(self):
+        self.rested = False
